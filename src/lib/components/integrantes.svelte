977fe7ea18a9528fb8e3593e1e68830e2f04cb99
peargenfiap <rm88246@fiap.com.br>
2023-11-19 11:20:07 -0300

    style: ajuste na estilização da seção integrantes

--- src/lib/components/integrantes.svelte
+++ src/lib/components/integrantes.svelte
@@ -8,10 +8,12 @@
 
 <section class="container mx-auto p-4">
 	<h3 class="text-white-700 mb-4 mt-3 flex items-center justify-center text-2xl font-semibold">Integrantes</h3>
-	{#each integrantes as integrante}
-		<div class="bg-surface-500-900-token m-2 mx-auto flex w-1/2 items-center justify-center rounded p-4 shadow">
-			<span>{integrante.nome}</span>
-			<span class="ml-3 text-red-400">{integrante.rm}</span>
-		</div>
-	{/each}
+	<div class="flex items-center justify-center">
+		{#each integrantes as integrante}
+			<div class="bg-surface-500-900-token m-2 mx-auto flex w-1/2 items-center justify-center rounded p-4 shadow">
+				<span>{integrante.nome}</span>
+				<span class="ml-3 text-red-400">{integrante.rm}</span>
+			</div>
+		{/each}
+	</div>
 </section>
